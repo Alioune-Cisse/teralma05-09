2b3de4a749c000321caf37dc9e339cbb7e852e0b
Alioune CISSE <60492447+Alioune-Cisse@users.noreply.github.com>
2022-09-07 14:44:31 +0000

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -13,22 +13,30 @@ def read_data(file, sep=",", header=0) -> pd.DataFrame:
     return data
 
 def clean_data(df: pd.DataFrame, col) -> pd.DataFrame:
+    df.iloc[:,0] =  pd.Series(df.iloc[:,0]).fillna(method='ffill')
     df = df.drop_duplicates().apply(lambda x: x.astype(str).str.lower())
     df = df[(df[col] != "nan") & (df[col] != "#value!")]
     return df.apply(lambda x: x.str.strip())
 
 def get_palier(df:pd.DataFrame, valeur:float) -> str:
+    
     paliers = df.iloc[-1:,2:].transpose().astype('float')
-    try:
-        palier = paliers.index[paliers.iloc[:,0]>=valeur][0]
-    except Exception as e:
-        if(valeur > paliers.iloc[-1,0]):
-            palier = paliers.index[-1]
-        elif(valeur < paliers.iloc[0,0]):
-            palier = paliers.index[0]
-        else:
-            print(e)
-    return palier
+    
+    if(valeur > paliers.iloc[-2,0]):
+            prev_ = next_ = paliers.index[-1]
+            #next_ = paliers.index[-1]
+            
+    elif(valeur < paliers.iloc[0,0]):
+            prev_ = next_ = paliers.index[0]
+            #next_ = paliers.index[0]
+            
+    else:
+        #prev_ = paliers.index[paliers.iloc[:,0]>=valeur][0]
+        #next_ = paliers.index[paliers.iloc[:,0]>=valeur][1]
+        prev_ = paliers.index[paliers.iloc[:,0]<=valeur][-1]
+        next_ = paliers.index[paliers.iloc[:,0]>valeur][0]
+        
+    return prev_, next_
 
 def choix_categ(df: pd.DataFrame, choix: list) -> dict:
     all_choice = df.iloc[:,1].unique().tolist()
@@ -37,30 +45,30 @@ def choix_categ(df: pd.DataFrame, choix: list) -> dict:
     return {"X"+str(i+1): choix_possibles[i] for i in range(len(choix_possibles))}
 
 def constraints(df: pd.DataFrame, budget: int, choice: list, invites: int) -> dict:
-    palier = get_palier(df, budget)
+    prev_, next_ = get_palier(df, budget)
     #print(palier)
-    contraintes = {"egalites": [], "inegalites": []}
+    contraintes = {}
     choix = choix_categ(df, choice)
 
     for it, elt in list(choix.items()):
-        prix = float(df[df.iloc[:,1] == elt][palier])
-        #valeurs.append(prix)
-        if list(df[df.iloc[:,1] == elt].iloc[:,0])[0] in ["super marché", "produits beautés"]:
-            contraintes["egalites"].append({elt: prix})
-        else:
-            contraintes["inegalites"].append({elt: prix})
+        minim = float(df[df.iloc[:,1] == elt][prev_])
+        maxim = float(df[df.iloc[:,1] == elt][next_])
+        #categ = df[df.iloc[:,1] == elt].iloc[:,0]
+        contraintes[elt] = [minim, maxim]
+        #print(categ)
 
 
     return contraintes
 
 
-def pulp_optimize(df: pd.DataFrame, budget: int, choice: list, invites: int) -> dict:
+def pulp_optimize(df:pd.DataFrame, budget: int, choice: list, invites: int) -> dict:
+ 
     contraintes = constraints(df, budget, choice, invites)
-    choix = choix_categ(df, choice)
-
+    choix = choix_categ(df,choice)
+    
     # Initialiser les variables
     variables = [LpVariable(i, lowBound=0, cat=LpInteger) for i in choix.keys()]
-
+    
     # Initialiser le problème
     probleme = LpProblem(name='Répartition budget', sense=LpMaximize)
 
@@ -69,49 +77,25 @@ def pulp_optimize(df: pd.DataFrame, budget: int, choice: list, invites: int) -> 
     probleme.add(contrainte_budget)
 
     for elt in variables:
-        # Ajouter contraintes des valeurs non nulles
-        # contrainte = LpConstraint(e=elt, sense=LpConstraintGE, name='not null contrainte ' + choix[elt.name], rhs=0)
-        # probleme.add(contrainte)
-
-        # Récupérer listes contraintes égalités et inégalités
-        contraintes_egalites = set().union(*(d.keys() for d in contraintes["egalites"]))
-        contraintes_inegalites = set().union(*(d.keys() for d in contraintes["inegalites"]))
-
-        if choix[elt.name] in contraintes_inegalites:
-            for sub in contraintes["inegalites"]:
-                try:
-                    valeur = sub[choix[elt.name]]
-                except:
-                    pass
-
-            # Ajouter contrainte d'inégalités
-            contrainte = LpConstraint(e=elt, sense=LpConstraintLE, name='contrainte ' + choix[elt.name], rhs=valeur)
-            probleme.add(contrainte)
-
-            contrainte_ = LpConstraint(e=elt, sense=LpConstraintGE, name='great contrainte ' + choix[elt.name],
-                                       rhs=valeur // 4)
-            probleme.add(contrainte_)
-
-        elif choix[elt.name] in contraintes_egalites:
-            for sub in contraintes["egalites"]:
-                try:
-                    valeur = sub[choix[elt.name]]
-                except:
-                    pass
-
-            # Ajouter contraintes d'égalités
-            contrainte = LpConstraint(e=elt, sense=LpConstraintEQ, name='contrainte ' + choix[elt.name], rhs=valeur)
-            probleme.add(contrainte)
+        #Ajouter contrainte d'inégalités
+        valeur_min = contraintes[choix[elt.name]][0]
+        valeur_max = contraintes[choix[elt.name]][1]
+        
+        contrainte = LpConstraint(e=elt, sense=LpConstraintGE, name='great contrainte ' + choix[elt.name], rhs=valeur_min)
+        probleme.add(contrainte)
+            
+        contrainte_ = LpConstraint(e=elt, sense=LpConstraintLE, name='less contrainte ' + choix[elt.name], rhs=valeur_max)
+        probleme.add(contrainte_)
 
     # Ajouter la fonction objectif à maximiser au problème
-    fonction_objectif = LpAffineExpression(e=sum(variables))
+    fonction_objectif = LpAffineExpression(e=lpSum(variables))
     probleme.setObjective(fonction_objectif)
 
     # Initialiser le solveur
     solver = PULP_CBC_CMD(timeLimit=20, msg=True)
     probleme.solve(solver=solver)
 
-    # print(probleme)
+    #print(probleme)
     repartitions = {}
     for val in variables:
         repartitions[choix[val.name]] = round(val.value())
